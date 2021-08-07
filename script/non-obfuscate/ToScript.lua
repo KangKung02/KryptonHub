7181802d95e7babd8f41329fc4093310e26e2867
KangKung <87695599+KangKung02@users.noreply.github.com>
2021-08-07 19:24:15 +0700

    Update ToScript.lua

--- script/non-obfuscate/ToScript.lua
+++ script/non-obfuscate/ToScript.lua
@@ -3,7 +3,7 @@ if game.PlaceId == 1499872953 then
  elseif game.PlaceId == 2116452049 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/2116452049.lua"))();
   elseif game.PlaceId == 2281639237 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/2281639237.lua"))();
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/KryptonHub/main/script/obfuscate/Stand_Online.lua"))();
   elseif game.PlaceId == 394506555 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/394506555.lua"))();
   elseif game.PlaceId == 4520749081 then
