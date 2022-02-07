d3e0269e7fda5a1d68c923c5ac70985460c90763
KangKung <87695599+KangKung02@users.noreply.github.com>
2022-02-07 22:24:28 +0700

    Update ToScript.lua

--- script/non-obfuscate/ToScript.lua
+++ script/non-obfuscate/ToScript.lua
@@ -22,5 +22,7 @@ if game.PlaceId == 1499872953 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/6381829480.lua"))();
   elseif game.PlaceId == 6574375780 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/6574375780.lua"))();
+  elseif game.PlaceId == 6329844902 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/H2O/main/Free-Uesr/6329844902.lua"))();
   else return error("this game did't support!", 2)
   end
